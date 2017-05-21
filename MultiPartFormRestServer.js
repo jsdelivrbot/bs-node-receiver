ba0444664d7a66873e3d1ec3fff6f23645fc29bf
santhoshvoltum <santhosh@voltum.com.au>
2017-05-22 09:49:11 +1000

    new file:   MultiPartFormRestServer.js 	new file:   package.json 	new file:   testsubmission.htm

---
+++ MultiPartFormRestServer.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var multer  = require('multer')
+
+var upload = multer({ dest: 'uploads/' })
+
+var app = express()
+
+app.listen(1357, function() {	
+	console.log('Listening at port 1357');
+});
+
+app.post('/funda/bsupload', upload.any(), function (req, res, next) {
+  // req.files is array of `photos` files
+  console.log(req.files);
+  // req.body will contain the text fields, if there were any
+  console.log(req.body);
+  res.send('matcha cupcake');
+})
